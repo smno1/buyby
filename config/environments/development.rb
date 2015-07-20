de66dbf7de272672ae2358a5f2db4b56e19ffc74
sheen <ysmsmno1@gmail.com>
2015-07-20 16:54:34 +1000

    mailer config mistake

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,15 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "buyby.au@gmail.com",
+    password: "SheenShawnJohnny"
+  }
   config.cache_classes = false
 
   # Do not eager load code on boot.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # email
-  config.action_mailer.default_url_options = { host: ENV['WASTE_MANAGEMENT_HOST']}
+  config.action_mailer.default_url_options = { host: ENV['HOST']}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true

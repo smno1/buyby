1a62a49824ad437eb5e60ff39396818e7a507403
sheen <ysmsmno1@gmail.com>
2015-07-21 00:36:47 +1000

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 
 
   # email
-  config.action_mailer.default_url_options = { host: 'http://buyby.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'http://buyby.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: "buyby.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "buyby.au@gmail.com",

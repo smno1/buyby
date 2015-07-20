05e060fd1e8b08008111fb5687fe7dfc5a45e09e
sheen <ysmsmno1@gmail.com>
2015-07-21 02:07:53 +1000

    mail production config change back to plain text

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
 
 
   # email
-  config.action_mailer.default_url_options = { host: ENV["HOST"]}
+  config.action_mailer.default_url_options = { host: 'http://buyby.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -13,11 +13,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "buyby.au@gmail.com",
+    password: "SheenShawnJohnny"
   }
 
   # Code is not reloaded between requests.

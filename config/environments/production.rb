8eb81d167813af86342efeace1405f27d9161180
sheen <ysmsmno1@gmail.com>
2015-07-21 01:08:22 +1000

    mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,16 +8,16 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: 25,
+    port: 587,
     domain: "gmail.com",
     authentication: "plain",
-    enable_starttls_auto: true,
     user_name: "buyby.au@gmail.com",
     password: "SheenShawnJohnny"
+    enable_starttls_auto: true,
   }
 
   # Code is not reloaded between requests.

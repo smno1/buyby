24f4995ab8a5e08bdc5286084d88d56e3dcc2b0c
sheen <ysmsmno1@gmail.com>
2015-07-22 19:09:38 +1000

    dependent delete

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,7 +2,7 @@ class Offer < ActiveRecord::Base
   belongs_to :request
   belongs_to :user
   belongs_to :status
-  has_many :comments
+  has_many :comments, :dependent=>:destroy
   has_many :received_comments, :class_name=>"Comment"
   has_many :received_gave_offer_comments, :class_name=>"Comment"
   scope :unread, -> {where :read=>false}
--- app/models/request.rb
+++ app/models/request.rb
@@ -3,7 +3,7 @@ class Request < ActiveRecord::Base
 	belongs_to :user
 	has_many :received_offers, :class_name=>"Offer"
 	has_many :received_comments, :through=>:offers
-	has_many :offers
+	has_many :offers, :dependent=>:destroy
 	has_one :status
 
 	scope :status_id, ->(status_id) {where :status_id=>status_id}
